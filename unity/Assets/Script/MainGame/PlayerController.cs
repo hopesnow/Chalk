a55c615f9ba8f89743e638d207ed4933462b3c60
r-hatase <r-hatase@landho.co.jp>
2018-09-01 14:08:29 +0900

    unityをとりあえずコミット

---
+++ unity/Assets/Script/MainGame/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Image player;
+
+    // 初期化処理
+    private void Start()
+    {
+    }
+
+    // 更新処理
+    private void Update()
+    {
+    }
+}
