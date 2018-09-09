61a4b30db12df95fbdf1b48baf6c3caeba3ad103
y-kajita <y-kajita@landho.co.jp>
2018-09-09 17:31:56 +0900

    描画の調整

--- unity/Assets/Script/MainGame/drawPhysicsLine.cs
+++ unity/Assets/Script/MainGame/drawPhysicsLine.cs
@@ -8,15 +8,15 @@ public class drawPhysicsLine : MonoBehaviour {
     public GameObject linePrefab;
     public float lineLength = 0.2f;
     public float lineWidth = 0.1f;
-    private List<Vector3> linePoints;
-    private List<GameObject> lines;
+    private List<Vector2> linePoints;
 
     private Vector3 touchPos;
+    private GameObject newLine;
 
     void Start()
     {
-        linePoints = new List<Vector3>();
-        lines = new List<GameObject>();
+        linePoints = new List<Vector2>();
+        newLine = null;
     }
 
     void Update()
@@ -59,8 +59,11 @@ public class drawPhysicsLine : MonoBehaviour {
                 //test
 
                 //GameObject newLine = new GameObject("Line"+linePoints.Count);
-                GameObject newLine = Instantiate(linePrefab);
-                newLine.name = "Line" + linePoints.Count;
+                if (newLine == null)
+                {
+                    newLine = Instantiate(linePrefab);
+                    newLine.name = "Line" + linePoints.Count;
+                }
                 LineRenderer lRend = newLine.GetComponent<LineRenderer>();// write line
                 lRend.startColor = Color.white;
                 lRend.positionCount = 2;
@@ -72,50 +75,105 @@ public class drawPhysicsLine : MonoBehaviour {
                 Rigidbody2D rigid2D = newLine.GetComponent<Rigidbody2D>();
                 rigid2D.gravityScale = 0f;
                 newLine.transform.parent = this.transform;
-                CapsuleCollider2D capsule = newLine.GetComponent<CapsuleCollider2D>();// set collision
-                //capsule.radius = lineWidth / 2;
-                capsule.size = new Vector2(lineLength/2, (endPos - startPos).magnitude);
-                //capsule.center = Vector3.zero;
-                capsule.offset = Vector2.zero;
-                //capsule.direction = CapsuleDirection2D.Vertical;//2; // Z-axis for easier "LookAt" orientation
-                capsule.transform.position = startPos + (endPos - startPos) / 2;
-                capsule.transform.LookAt(startPos);
-                //capsule.height = (endPos - startPos).magnitude;
+                PolygonCollider2D polygon = newLine.GetComponent<PolygonCollider2D>();
 
-                lines.Add(newLine);
+
                 linePoints.Add(endPos);
+                WriteLine2D(lRend, polygon);
                 touchPos = endPos;
                 //
             }
         }
     }
 
-    void WriteLine2D()
+    void WriteLine2D(LineRenderer lRend, PolygonCollider2D polygon)
     {
+        lRend.positionCount = linePoints.Count;
         for (int i = 0; i + 1 <= linePoints.Count;i++)
         {
-            
+            lRend.SetPosition(i, linePoints[i]);
         }
+        List<Vector2> setPointList = new List<Vector2>();
+        //上の線
+        for (int i = -1; i < linePoints.Count; i++)
+        {
+            setPointList.Add(CalcSetPoint(i, linePoints, false));
+        }
+
+        //下の線
+        for (int i = linePoints.Count-1; i >= -1; i--)
+        {
+            setPointList.Add(CalcSetPoint(i, linePoints, true));
+        }
+
+        //コリジョン設定
+        /*gameObject.GetComponent<PolygonCollider2D>()*/polygon.points = setPointList.ToArray();
+
 
     }
 
     void ClearLines()
     {
-        for (int i = 0; i < lines.Count; i++)// Clear Object
-        {
-            Destroy(lines[i]);
-        }
         linePoints.Clear();// Clear List
-        lines.Clear();
+        Destroy(newLine);
     }
 
-	private void OnCollisionEnter2D(Collision2D collision)
-	{
-        Debug.Log("enter2d");
-	}
+    //設定地点を計算する
+    private Vector2 CalcSetPoint(int i, List<Vector2> pointList, bool isBottomPoint)
+    {
 
-	private void OnCollisionEnter(Collision collision)
-	{
-        Debug.Log("enter");
-	}
+        Vector2 point1, point2;
+        Debug.Log("pointList.Count=" + pointList.Count + ", i=" + i);
+
+        if (i <= 0)
+        {
+            point1 = pointList[0];
+            point2 = pointList[1];
+        }
+        else
+        {
+            point1 = pointList[i - 1];
+            point2 = pointList[i];
+        }
+
+        //前回の点、今回の点の2点間の角度を求め、
+        //さらそこに90度加算することで、
+        //2点間にかかる線分に下りる垂線と平行にになる線(前回の点から線を引いて)の角度を求める
+        float aim = GetAim(point2, point1);
+        float correctionAim = 90;
+
+        if (isBottomPoint)
+        {
+            correctionAim *= -1;
+        }
+
+        if (point1.y < point2.y)
+        {
+            aim = GetAim(point1, point2);
+            correctionAim *= -1;
+        }
+
+        aim += correctionAim;
+
+        //前回の点を角度の方向に移動
+        Vector2 setPoint = Vector2.zero;
+        if (i == -1)
+        {
+            point2 = point1;
+        }
+
+        setPoint.x = (float)(point2.x + 5*lineWidth * 0.5f * Mathf.Cos(Mathf.Deg2Rad * aim));
+        setPoint.y = (float)(point2.y + 5*lineWidth * 0.5f * Mathf.Sin(Mathf.Deg2Rad * aim));
+
+        return setPoint;
+    }
+
+    //p2からp1への角度を求める
+    private float GetAim(Vector2 p1, Vector2 p2)
+    {
+        Vector2 a = new Vector2(1, 0);
+        Vector2 b = p2 - p1;
+        return Vector2.Angle(a, b);
+    }
+
 }
