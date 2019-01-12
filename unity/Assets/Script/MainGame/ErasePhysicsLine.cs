3f16b311e3a22e04f157ae8c6e6720cfd22ce746
y-kajita <y-kajita@landho.co.jp>
2019-01-12 15:03:30 +0900

    選択した線の太さが変わるように調整

--- unity/Assets/Script/MainGame/ErasePhysicsLine.cs
+++ unity/Assets/Script/MainGame/ErasePhysicsLine.cs
@@ -27,12 +27,31 @@ public class ErasePhysicsLine : MonoBehaviour {
 		
 	}
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Line")
+        {
+            collision.GetComponent<LineRenderer>().widthMultiplier = 2;
+
+        }
+    }
+
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (isErase && collision.tag == "Line")
+        if (collision.tag == "Line")
         {
             Debug.Log("Erase Collision:" + collision.gameObject.name);
-            Destroy(collision.gameObject);
+            if(isErase)
+                Destroy(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Line")
+        {
+            collision.GetComponent<LineRenderer>().widthMultiplier = 1;
+
         }
     }
 }
