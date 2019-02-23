d015cd897db2607f4faecf7724fcf57b032225f7
y-kajita <y-kajita@landho.co.jp>
2019-02-23 13:52:00 +0900

    チョーク線の判定を調整

--- unity/Assets/Script/MainGame/DrawPhysicsLine.cs
+++ unity/Assets/Script/MainGame/DrawPhysicsLine.cs
@@ -141,8 +141,8 @@ public class DrawPhysicsLine : MonoBehaviour
         var tc = (ax - bx) * (cy - ay) + (ay - by) * (ax - cx);
         var td = (ax - bx) * (dy - ay) + (ay - by) * (ax - dx);
 
-        // return tc * td < 0 && ta * tb < 0;
-        return tc * td <= 0 && ta * tb <= 0; // 端点を含む場合
+        return tc * td < 0 && ta * tb < 0;
+        // return tc * td <= 0 && ta * tb <= 0; // 端点を含む場合
     }
 
     /** ********************************************************************************
