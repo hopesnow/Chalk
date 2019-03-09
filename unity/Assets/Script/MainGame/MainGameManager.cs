c8e201db0071617afb0c5460e566010c542f5ea9
r-hatase <r-hatase@landho.co.jp>
2019-03-09 14:57:48 +0900

    ステージ選択用の処理を準備

--- unity/Assets/Script/MainGame/MainGameManager.cs
+++ unity/Assets/Script/MainGame/MainGameManager.cs
@@ -7,25 +7,25 @@ public class MainGameManager : MonoBehaviour
     [SerializeField] private Camera mainCamera;
     [SerializeField] private PlayerController[] players;
 
+    [SerializeField] private Transform stageParent;
+    [SerializeField] private GameObject[] stages;
+
     [SerializeField] private Text textOrigin;
 
-    // 初期化処理
-    private void Awake()
-    {
-    }
-
-    // 初期化処理
+    /** ********************************************************************************
+     * @summary 初期化処理
+     ***********************************************************************************/
     private void Start()
     {
         PlayerController[] test = FindObjectsOfType<PlayerController>();
-        players = test;
-        for (int i = 0; i < players.Length; i++)
-        {
-            PlayerController player = players[i];
-            this.textOrigin.gameObject.SetActive(false);
-            this.mainCamera.orthographicSize = 3.6f;
+        this.players = test;
 
-            // ゴール判定
+        this.textOrigin.gameObject.SetActive(false);
+        this.mainCamera.orthographicSize = 3.6f;
+
+        foreach (var player in this.players)
+        {
+            // ゴール判定オブザーバーの登録
             player.IsGoal.ObserveEveryValueChanged(x => x.Value).Subscribe(goal =>
             {
                 if (goal)
@@ -40,24 +40,30 @@ public class MainGameManager : MonoBehaviour
         Log("ELECOM DIRECT INPUT ONLY NOW.");
     }
 
-    // 更新処理
+    /** ********************************************************************************
+     * @summary 更新処理
+     ***********************************************************************************/
     private void Update()
     {
         if (Input.GetButtonDown("DebugReset"))
         {
-            for (int i = 0; i < players.Length; i++)
+            foreach (var player in this.players)
             {
-                PlayerController player = players[i];
                 player.Reset();
-                Log("GameReset... !");
             }
+
+            Log("GameReset... !");
         }
+
         if (Input.GetButtonDown("Pause"))
         {
             Debug.Log("Pause");
         }
     }
 
+    /** ********************************************************************************
+     * @summary ログ出力処理
+     ***********************************************************************************/
     public void Log(string str)
     {
         Debug.LogFormat("{0}", str);
@@ -69,5 +75,27 @@ public class MainGameManager : MonoBehaviour
         log.text = str;
 
         this.textOrigin.gameObject.SetActive(false);
+    }
+
+    /** ********************************************************************************
+     * @summary ステージ選択処理(number: 0から)範囲外だとランダムになる
+     ***********************************************************************************/
+    public void SetStage(int number = -1)
+    {
+        // 前回ステージを削除
+        foreach (Transform stageChild in this.stageParent)
+        {
+            Destroy(stageChild.gameObject);
+        }
+
+        // 範囲外チェック
+        if (number < 0 || number > this.stages.Length - 1)
+        {
+            // ランダムに選ぶ
+            number = Random.Range(0, this.stages.Length);   // stages.Lengthは含まない(intだから)
+        }
+
+        // ステージ生成
+        Instantiate(this.stages[number], this.stageParent);
     }
 }
