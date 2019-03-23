ce355d112d1ed2e9df6bba0bde527d78cc48d7ca
r-hatase <r-hatase@landho.co.jp>
2019-03-23 12:38:55 +0900

    ステージオブジェクトにゴール用のオブジェクトを追加

--- unity/Assets/Script/MainGame/StageController.cs
+++ unity/Assets/Script/MainGame/StageController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class StageController : MonoBehaviour
 {
     [SerializeField] private Transform[] startObj = new Transform[4];
+    [SerializeField] private Transform[] goalObj = new Transform[4];
 
     public Vector3[] StartPos { get { return this.startObj.Select(l => l.position).ToArray(); } }
+    public Vector3[] GoalPos { get { return this.startObj.Select(l => l.position).ToArray(); } }
 }
